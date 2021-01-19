20b357eb7031a3d224d64f0a712f66c748840614
nsgamliel <nsgamliel@gmail.com>
2021-01-19 13:46:43 -0600

    bugsss

--- L1/src/parser.cpp
+++ L1/src/parser.cpp
@@ -453,12 +453,12 @@ namespace L1 {
   struct Instruction_rule:
     pegtl::sor<
       pegtl::seq< pegtl::at<Instr_return_rule>    , Instr_return_rule     >,
+      pegtl::seq< pegtl::at<instr_comp_rule>      , instr_comp_rule       >,
       pegtl::seq< pegtl::at<Instr_assignment_rule>, Instr_assignment_rule >,
       pegtl::seq< pegtl::at<Instr_label_defn_rule>, Instr_label_defn_rule >,
       pegtl::seq< pegtl::at<instr_aop_rule>       , instr_aop_rule        >,
       pegtl::seq< pegtl::at<instr_sop_rule>       , instr_sop_rule        >,
-      pegtl::seq< pegtl::at<instr_dir_jump_rule>  , instr_dir_jump_rule   >,
-      pegtl::seq< pegtl::at<instr_comp_rule>      , instr_comp_rule       >
+      pegtl::seq< pegtl::at<instr_dir_jump_rule>  , instr_dir_jump_rule   >
     > {};
 
   struct Instructions_rule:
