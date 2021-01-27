4c89013ec268d31177631a4beb0a3c5578654929
nsgamliel <nsgamliel@gmail.com>
2021-01-26 23:29:24 -0600

    asdf

--- L2/src/liveness.cpp
+++ L2/src/liveness.cpp
@@ -161,7 +161,7 @@ namespace L2 {
 				for (int succ : f_l.instructions[i]->successors) {
 					if (printV) std::cout << "successor " << succ << std::endl;
 					for (size_t elem : f_l.instructions[succ]->in)
-						new_set.push_back(elem);
+						if (elem >= 0) new_set.push_back(elem);
 				}
 
 				if (printV) std::cout << "checking for dirty" << std::endl;
