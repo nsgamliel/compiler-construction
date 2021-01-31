831d8d9d80db520f09bb11ee07f6397b440ac65b
nsgamliel <nsgamliel@gmail.com>
2021-01-31 16:36:00 -0600

    asdf

--- L2/src/interference.cpp
+++ L2/src/interference.cpp
@@ -8,8 +8,21 @@ namespace L2 {
 		Function_i* f_i;
 		f_i->i_graph = interference_graph_setup(f_l);
 
+		std::vector<size_t> registers_hash;
+		for (auto str : f_l->callee_save)
+			registers_hash.push_back(f_l->str_hash(str));
+		for (auto str : f_l->caller_save)
+			registers_hash.push_back(f_l->str_hash(str));
+
 		// check for gp registers
-		// 
+		for (auto item : f_l->items_l) {
+			if (registers_hash.find(item.first) != registers_hash.end()) {
+				for (auto x : registers_hash)
+					f_i->i_graph.adj_matrix[f_i->i_graph.indices[item.first]*f_i->i_graph.indices.size() + f_i->i_graph.indices[x]] = true;
+			}
+		}
+
+		
 	}
 
 	Interference_graph interference_graph_setup(L2::Function_l* f_l) {
