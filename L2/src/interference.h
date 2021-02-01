a3b3194108ddf3cc7592d65b8ce7e3fff698d651
nsgamliel <nsgamliel@gmail.com>
2021-01-31 19:27:11 -0600

    asdf

--- L2/src/interference.h
+++ L2/src/interference.h
@@ -22,7 +22,7 @@ namespace L2 {
 
 	struct Function_i {
 		Interference_graph i_graph;
-		std::map<size_t, std::string>* items_i;
+		std::map<size_t, std::string> items_i;
 	};
 
 	// main entry point for interference analysis
