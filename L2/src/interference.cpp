0d822b849e1d95910aa8bf42c0feb3af4c2cbdf8
nsgamliel <nsgamliel@gmail.com>
2021-01-31 20:44:40 -0600

    asdf

--- L2/src/interference.cpp
+++ L2/src/interference.cpp
@@ -139,6 +139,7 @@ namespace L2 {
 	}
 
 	void generate_interference_output(Function_i f_i, L2::Function_l f_l) {
+		std::cout << "\n\n\n" << f_i.items_i[f_i.i_graph.hashes[0]] << "\n\n\n";
 		for (int i=0; i<f_i.i_graph.indices.size(); i++) {
 			std::cout << f_i.items_i[f_i.i_graph.hashes[i]];
 			for (int j=1; j<f_i.i_graph.indices.size(); j++) {
