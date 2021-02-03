0b2bcb18650a31d3123e650c7d876d8022d5708d
nsgamliel <nsgamliel@gmail.com>
2021-02-03 09:22:03 -0600

    asdf

--- L2/src/spiller.cpp
+++ L2/src/spiller.cpp
@@ -53,7 +53,7 @@ namespace L2 {
 					if (found_match) {
 						var_replaced = true;
 						if (printS) std::cout << "found match" << std::endl;
-						if (f_s.num_replace > 0 && std::find(f_l.instructions[ind]->gen.begin(), f_l.instructions[ind]->gen.end(), f_l.str_hash(var)) != f_l.instructions[ind]->gen.end()) {
+						if (f_s.num_replace > -1 && std::find(f_l.instructions[ind]->gen.begin(), f_l.instructions[ind]->gen.end(), f_l.str_hash(var)) != f_l.instructions[ind]->gen.end()) {
 							if (printS) std::cout << "loading previous store" << std::endl;
 							auto load_instr = new L2::Instruction();
 							load_instr->op = load;
